27b3a6e999913ab26d28172d086ccbb68fb6567c
sebotypo <sebotypo@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-07 11:40:02 +0000

    Disabled pi2 and pi3 for the moment

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,18 +18,18 @@ t3lib_extMgm::addTypoScript($_EXTKEY,'setup','
 
 
   ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_sevenpack_pi2 = < plugin.tx_sevenpack_pi2.CSS_editor
-',43);
+//t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
+//	tt_content.CSS_editor.ch.tx_sevenpack_pi2 = < plugin.tx_sevenpack_pi2.CSS_editor
+//',43);
 
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_sevenpack_pi2.php','_pi2','list_type',1);
+//t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_sevenpack_pi2.php','_pi2','list_type',1);
 
 
   ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_sevenpack_pi3 = < plugin.tx_sevenpack_pi3.CSS_editor
-',43);
+//t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
+//	tt_content.CSS_editor.ch.tx_sevenpack_pi3 = < plugin.tx_sevenpack_pi3.CSS_editor
+//',43);
 
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi3/class.tx_sevenpack_pi3.php','_pi3','list_type',1);
+//t3lib_extMgm::addPItoST43($_EXTKEY,'pi3/class.tx_sevenpack_pi3.php','_pi3','list_type',1);
 
 ?>
